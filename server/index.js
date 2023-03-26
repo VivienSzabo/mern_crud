df2b705d41e7f8bb33694a752d1d4cda0205a20a
VivienSzabo <vivienkorosiszabo@gmail.com>
2023-03-26 13:08:00 +0200

    test data save in DB

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3001;
+
+const FoodeModel = require("./models/Food");
+
+app.use(express.json());
 
 mongoose.connect()
 
 
+app.get('/', async(req, res) => {
+    const food = new FoodeModel({
+        foodName:"Apple pie", 
+        daysSinceIAte:3});
 
+    try{
+        await food.save();
+        res.send("insert data")
+    }catch(err){
+        console.log(err)
+    }
+});
 
 
 
